23d92aaa07cd6eeb67bf33fa4577a109ce10a401
aditya90205 <adityasrivastav90205@gmail.com>
2025-02-20 13:27:53 +0530

    project setup

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      
+      Hello Aditya Srivastav
+      <br />
+
+
+      <Button>Fuck Me</Button>
+    </div>
+
+  );
+}
