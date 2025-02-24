65397cc2c61135ace3b1cd622cb60340afd50d30
aditya90205 <adityasrivastav90205@gmail.com>
2025-02-24 17:46:42 +0530

    Completed the Landing Page Design

---
+++ data/stats.js
@@ -0,0 +1,19 @@
+export const stats = [
+  {
+    title: "50+",
+    description: "Interview Question",
+  },
+  {
+    title: "1000+",
+    description: "Interview Questions",
+  },
+  {
+    title: "95%",
+    description:
+      "Success Rate",
+  },
+  {
+    title: "24/7",
+    description: "AI Support",
+  },
+];
