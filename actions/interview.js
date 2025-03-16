cdf760f22104fab832eb6904fb89e81c121bd404
aditya90205 <adityasrivastav90205@gmail.com>
2025-03-16 16:07:21 +0530

    Add Interview Prep Functionaility

--- actions/interview.js
+++ actions/interview.js
@@ -111,23 +111,51 @@ export async function saveQuizResult(questions, answers, score) {
       const response = result.response;
       improvementTip = response.text().trim();
     } catch (error) {
-        console.error("Error generating improvement tip:", error);
+      console.error("Error generating improvement tip:", error);
     }
   }
-    
-    try {
-        const assessment = await db.assessment.create({
-            data: {
-                userId: user.id,
-                quizScore: score,
-                questions: questionResults,
-                category: "Technical",
-                improvementTip,
-            }
-        });
-        return assessment;
-    } catch (error) {
-        console.error("Error saving quiz result:", error);
-        throw new Error("Failed to save quiz result");
-    }
+
+  try {
+    const assessment = await db.assessment.create({
+      data: {
+        userId: user.id,
+        quizScore: score,
+        questions: questionResults,
+        category: "Technical",
+        improvementTip,
+      },
+    });
+    return assessment;
+  } catch (error) {
+    console.error("Error saving quiz result:", error);
+    throw new Error("Failed to save quiz result");
+  }
+}
+
+export async function getAssessments() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: {
+      clerkUserId: userId,
+    },
+  });
+
+  if (!user) throw new Error("User not found");
+
+  try {
+    const assessments = await db.assessment.findMany({
+      where: {
+        userId: user.id,
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+    return assessments;
+  } catch (error) {
+    console.error("Error fetching assessments:", error);
+    throw new Error("Failed to fetch assessments");
+  }
 }
--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { getAssessments } from "@/actions/interview";
+import React from "react";
+import StatsCards from "./_components/StatsCards";
+import PerformanceChart from "./_components/PerformanceChart";
+import QuizList from "./_components/QuizList";
 
-const InterviewPage = () => {
+const InterviewPage = async () => {
+  const assessments = await getAssessments();
   return (
-    <div>InterViewPage</div>
-  )
-}
+    <div>
+      <h1 className="text-6xl font-bold gradient-title mb-5">
+        Interview Preparation
+      </h1>
+      <div className="space-y-6">
+        <StatsCards assessments={assessments} />
+        <PerformanceChart assessments={assessments} />
+        <QuizList assessments={assessments} />
+      </div>
+    </div>
+  );
+};
 
 export default InterviewPage;
